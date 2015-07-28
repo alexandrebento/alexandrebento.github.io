65577df154a50f7354df94c2d1a9ea067711adea
Alex <alexandrebento@users.noreply.github.com>
2015-07-28 00:44:46 -0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,3 @@
 $(document).ready(function(){
-    
     var person = prompt("Please enter your name", "Harry Potter");
-    
-    $('.logo').click(function(){
-        $(this).effect('bounce', {times:3}, 500);    
-    });
 });
