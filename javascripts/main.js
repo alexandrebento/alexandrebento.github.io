1b5080b215f23d572cf04cee362872e83b2c5d53
Alex <alexandrebento@users.noreply.github.com>
2015-07-28 15:38:36 -0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  confirm('4');
+  confirm('1');
   $('.logo').click(function(){
     $(this).effect('bounce',{times:3},300);
     $(this).css('box-shadow','10px 10px 5px grey');
