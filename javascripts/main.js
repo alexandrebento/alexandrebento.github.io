198b76a7f0449450281bbd7522c573c7d0625161
Alex <alexandrebento@users.noreply.github.com>
2015-07-28 15:38:26 -0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
   confirm('4');
   $('.logo').click(function(){
     $(this).effect('bounce',{times:3},300);
-    $(this).css('background-color','yellow');
+    $(this).css('box-shadow','10px 10px 5px grey');
   });
 });
