65f53243fda9fd81717ac8a4b127a3e21fdac6a2
Alex <alexandrebento@users.noreply.github.com>
2015-07-28 15:20:30 -0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  confirm('3');
+  confirm('4');
   $('.logo').click(function(){
     $(this).effect('bounce',{times:3},300);
     $(this).css('background-color','yellow');
