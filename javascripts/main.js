214e409cad16d09240afef3714c24ae51c21b27d
Alex <alexandrebento@users.noreply.github.com>
2015-07-28 22:52:32 -0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-  confirm('3');
-  $('img').click(function(){
+  confirm('1');
+  $('div').click(function(){
     $(this).effect('bounce',{times:3},300);
     $(this).css('box-shadow','10px 10px 5px yellow');
   });
