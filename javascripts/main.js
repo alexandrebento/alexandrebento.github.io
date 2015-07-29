dbb845ea6edbceb9e2752dcc473db8db262f8851
Alex <alexandrebento@users.noreply.github.com>
2015-07-28 22:54:37 -0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-  confirm('1');
   $('div').click(function(){
     $(this).effect('bounce',{times:3},300);
-    $(this).css('box-shadow','10px 10px 5px yellow');
+    $(this).css('color','yellow');
   });
+  confirm('1');
 });
