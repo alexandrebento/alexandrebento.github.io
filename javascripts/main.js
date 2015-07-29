cf67b76ff550e7ffa3f6b691e7e74c4e3057e753
Alex <alexandrebento@users.noreply.github.com>
2015-07-28 23:02:40 -0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,7 @@
-$(document).ready(function(){
+
   $("div").toggle(function () {
       $("div").css({color: "black"});
   }, function () {
       $("div").css({color: "yellow"});
   });
   confirm('11');
-});
