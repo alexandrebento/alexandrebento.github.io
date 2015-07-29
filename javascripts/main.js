8d6831c741bb593014054298aefac9966f45f744
Alex <alexandrebento@users.noreply.github.com>
2015-07-28 23:15:49 -0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 $('.js').click(function() {
-  $(this).toggle( "bounce", { times: 3 }, "slow" );
+  $(this).effect( "bounce", { times: 3 }, "slow" );
 });
   confirm('1');
 });
