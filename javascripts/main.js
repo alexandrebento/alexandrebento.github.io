239e3c47ee37c2dc3bb50598c7166e51f4958308
Alex B <bento.alexandre@gmail.com>
2015-07-30 01:40:08 +0000

    update

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function(){
-    $('div.orb').click(function(){
-        $(this).effect('bounce', {times:3}, 500);    
-    });
-});
-=======
 $(document).ready(function() {
   $(".js").click(function(){
      $(this).css( "background", "yellow" );
@@ -12,4 +5,3 @@ $(document).ready(function() {
   });
   confirm('1');
 });
->>>>>>> fc744bf3d1dca565269602d22a45ba1c1f8502a8
