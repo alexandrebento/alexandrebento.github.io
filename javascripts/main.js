e540189b741b2a2568995882aeb52bb253a5bda1
Anonymous <anonymous@noemail.com>
2015-09-27 19:05:20 +0000

    double red

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
   	 $(this).css("background","#FF1E00");
   	 $(this).css("box-shadow","0px 0px 10px red");
   });
+    $(".satellite").click(function(){
+  	 $(this).css("background","#FF1E00");
+  	 $(this).css("box-shadow","0px 0px 10px red");
+  });
 });
