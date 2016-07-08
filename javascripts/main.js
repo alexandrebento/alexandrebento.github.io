04683ad73684bf1969f209b207586a08963e918c
Alex <bento.alexandre@gmail.com>
2016-07-08 17:41:42 -0300

    cleanup

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,19 +1,20 @@
 $(document).ready(
-function() {
-  $(".earth").hover(
-  function(){
-  	 $(this).css("box-shadow","0px 0px 100px blue");
-  },
-  function(){
-     $(this).css("box-shadow","0px 0px 10px yellow");
-  }
-  );
-  $(".satellite").hover(
-  function(){
-  	 $(this).css("box-shadow","0px 0px 25px red");
-  },
-  function(){
-     $(this).css("box-shadow","0px 0px 1px white");
-  }
-  );
-});
+    function () {
+        $(".earth").hover(
+            function () {
+                $(this).css("box-shadow", "0px 0px 100px blue");
+            },
+            function () {
+                $(this).css("box-shadow", "0px 0px 10px yellow");
+            }
+        );
+        $(".satellite").hover(
+            function () {
+                $(this).css("box-shadow", "0px 0px 25px red");
+            },
+            function () {
+                $(this).css("box-shadow", "0px 0px 1px white");
+            }
+        );
+    }
+);
